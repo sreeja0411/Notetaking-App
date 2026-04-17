8cc16735e21e3ce896cebc99e16b5e01a29bf44c
Tejashwini0123 <tejashwinichary67@gmail.com>
2026-04-17 10:14:01 +0530

    Base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
